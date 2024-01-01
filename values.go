bb80eadf468fbc9f796313638a772c16b72d6ef9
Suman Khadka <sumankhadka00@gmail.com>
2024-01-01 23:43:52 +0545

    values

---
+++ values.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("2+3 =", 2+3)
+	fmt.Println("5.0/7.0 =", 5.0/7.0)
+	fmt.Println(true && false)
+	fmt.Println(false && true)
+	fmt.Println(true || false)
+	fmt.Println(false || true)
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
--- variable.go
+++ variable.go
@@ -1,9 +1,31 @@
 package main
+
 import "fmt"
 
+// func main() {
+// 	var x float64
+// 	x = 20.0
+// 	fmt.Println(x)
+// 	fmt.Printf("x is of type %T\n", x)
+// }
+
 func main() {
-	var x float64
-	x = 20.0
-	fmt.Println(x)
-	fmt.Printf("x is of type %T\n", x)
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var g int = 5
+	fmt.Println(g)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int = 10
+	fmt.Println(e)
+
+	f := "apple"
+	fmt.Println(f)
+
 }
